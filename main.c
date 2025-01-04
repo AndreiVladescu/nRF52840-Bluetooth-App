a841a143f11758d7b48a7a916eb0a3166be28f89
Andrei Vladescu <rudolfal2lea@yahoo.com>
2025-01-04 22:36:47 +0200

    Initialization of files

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "boards.h"
+
+int main(void)
+{
+	bsp_board_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS);
+	while (true)
+	{
+		if (bsp_board_button_state_get(BSP_BUTTON_0))
+		{
+			bsp_board_led_on(BSP_BOARD_LED_2);
+		}
+		else
+		{
+			bsp_board_led_off(BSP_BOARD_LED_2);
+		}
+	}
+}
